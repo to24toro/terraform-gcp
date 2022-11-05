017d18a936de80e6ddfc562373f4ce05d33b515e
to24toro <to24toro@gmail.com>
2022-11-05 09:33:58 +0900

    modify iam binding

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -18,6 +18,10 @@ data "google_iam_policy" "owner_github_actions" {
 
 resource "google_service_account_iam_policy" "binding_sa_and_wi_github_actions" {
   service_account_id = google_service_account.github_actions.name
-  policy_data        = data.google_iam_policy.owner_github_actions.policy_data
+  policy_data        = data.google_iam_policy.workload_identity_user_github_actions.policy_data
 }
 
+resource "google_service_account_iam_policy" "binding_sa_and_owner_github_actions" {
+  service_account_id = google_service_account.github_actions.name
+  policy_data        = data.google_iam_policy.owner_github_actions.policy_data
+}
